b35f5a14c63562a4bd5caff6a32e343589287920
Warizz Yutanan <warizz.yutanan@gmail.com>
2018-10-18 07:17:44 +0700

    Fix `eslint` error (react/jsx-filename-extension)

---
+++ src/App.jsx
@@ -0,0 +1,129 @@
+// @flow
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+import fetch from 'isomorphic-fetch';
+
+import { summaryDonations } from './helpers';
+
+const Card = styled.div`
+  margin: 10px;
+  border: 1px solid #ccc;
+`;
+
+type Props = {};
+
+type State = {};
+
+class App extends Component<Props, State> {
+  constructor(props) {
+    super();
+
+    this.state = {
+      charities: [],
+      selectedAmount: 10,
+    };
+  }
+
+  componentDidMount() {
+    const self = this;
+    fetch('http://localhost:3001/charities')
+      .then(resp => resp.json())
+      .then(data => {
+        self.setState({ charities: data });
+      });
+
+    fetch('http://localhost:3001/payments')
+      .then(resp => resp.json())
+      .then(data => {
+        self.props.dispatch({
+          type: 'UPDATE_TOTAL_DONATE',
+          amount: summaryDonations(data.map(item => item.amount)),
+        });
+      });
+  }
+
+  render() {
+    const self = this;
+    const cards = this.state.charities.map((item, i) => {
+      const payments = [10, 20, 50, 100, 500].map((amount, j) => (
+        <label key={j}>
+          <input
+            name="payment"
+            onClick={function() {
+              self.setState({ selectedAmount: amount });
+            }}
+            type="radio"
+          />{' '}
+          {amount}
+        </label>
+      ));
+
+      return (
+        <Card key={i}>
+          <p>{item.name}</p>
+          {payments}
+          <button
+            onClick={handlePay.call(
+              self,
+              item.id,
+              self.state.selectedAmount,
+              item.currency,
+            )}
+          >
+            Pay
+          </button>
+        </Card>
+      );
+    });
+
+    const style = {
+      color: 'red',
+      margin: '1em 0',
+      fontWeight: 'bold',
+      fontSize: '16px',
+      textAlign: 'center',
+    };
+    const donate = this.props.donate;
+    const message = this.props.message;
+
+    return (
+      <div>
+        <h1>Tamboon React</h1>
+        <p>All donations: {donate}</p>
+        <p style={style}>{message}</p>
+        {cards}
+      </div>
+    );
+  }
+}
+
+export default connect(state => state)(App);
+
+function handlePay(id, amount, currency) {
+  const self = this;
+  return function() {
+    fetch('http://localhost:3001/payments', {
+      method: 'POST',
+      body: `{ "charitiesId": ${id}, "amount": ${amount}, "currency": "${currency}" }`,
+    })
+      .then(resp => resp.json())
+      .then(() => {
+        self.props.dispatch({
+          type: 'UPDATE_TOTAL_DONATE',
+          amount,
+        });
+        self.props.dispatch({
+          type: 'UPDATE_MESSAGE',
+          message: `Thanks for donate ${amount}!`,
+        });
+
+        setTimeout(() => {
+          self.props.dispatch({
+            type: 'UPDATE_MESSAGE',
+            message: '',
+          });
+        }, 2000);
+      });
+  };
+}
--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+
 import App from './App';
 
-const store = createStore(function(state, action) {
-  const _state = state == null ? {
-    donate: 0,
-    message: '',
-  } : state;
+const store = createStore((state, action) => {
+  const _state =
+    state == null
+      ? {
+          donate: 0,
+          message: '',
+        }
+      : state;
 
   switch (action.type) {
     case 'UPDATE_TOTAL_DONATE':
@@ -20,7 +24,8 @@ const store = createStore(function(state, action) {
         message: action.message,
       });
 
-    default: return _state;
+    default:
+      return _state;
   }
 });
 
@@ -28,5 +33,5 @@ render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
